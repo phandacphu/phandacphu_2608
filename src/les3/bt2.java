ddbe8f4f3ed25968dffcffea168d3d1c9b76e4f0
PDACPHU\ACER <phandacphu2201@gmail.com>
2026-01-05 21:34:42 +0700

    Initial commit

---
+++ src/les3/bt2.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package les3;
+
+import java.util.Scanner;
+
+public class bt2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ho va ten: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Student ID: ");
+        int ID = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Hometown: ");
+        String home = scanner.nextLine();
+
+        System.out.print("GPA: ");
+        float gpa = scanner.nextFloat();
+        scanner.nextLine(); 
+
+        System.out.print("Major: ");
+        String major = scanner.nextLine();
+
+        System.out.print("Scholarship (true/false): ");
+        boolean scho = scanner.nextBoolean();
+
+        System.out.println("\n----- THONG TIN SINH VIEN -----");
+        System.out.println("Ho va ten: " + name);
+        System.out.println("MSSV: " + ID);
+        System.out.println("Hometown: " + home);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Major: " + major);
+        System.out.println("Scholarship: " + scho);
+    }
+}
+
---
+++ src/les3/sl18.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package les3;
+
+import java.util.Scanner;
+
+public class sl18 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a sentence : ");
+        String sentence = keyboard.nextLine(); 
+    }
+}
